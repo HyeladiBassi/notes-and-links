f9b2520a71afc63d148628ac42a56dd5ec626c62
Temi <kolugbake@hotmail.com>
2023-06-18 18:51:42 +0100

    add splash screen

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,17 @@
 import { CssBaseline, Box, Container } from '@mui/material';
+import Splash from './components/Splash';
+import useSplash from './hooks/useSplash';
 
 const App = () => {
+  const loading = useSplash();
+
+  if (loading) return <Splash />;
+
   return (
     <>
       <CssBaseline />
-      <Container fixed>
-        <Box sx={{ backgroundColor: '#cfe8fc', height: '100vh' }} />
+      <Container maxWidth="sm" sx={{ p: 0 }}>
+        <Box sx={{ bgcolor: '#cfe8fc', height: '100vh' }} />
       </Container>
     </>
   );
